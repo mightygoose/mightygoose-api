eaad741495453e01fb7b83d173eb4de13e43177c
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2019-09-10 15:29:22 +0200

    add apollo server

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const Koa = require('koa');
 const { PORT } = require('./config');
+const ApolloServer = require('./graphql');
 
 
 const app = new Koa();
+
+ApolloServer.applyMiddleware({ app });
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });
 
-app.listen(PORT);
-console.log(`server is running on port ${PORT}`);
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
