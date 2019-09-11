eb22bfb9637d27c67051e5e4fdaf641447147be0
Alexander Zhukov <alexander.zhukov@native-instruments.de>
2019-09-11 13:45:06 +0200

    add example search resolver

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 coming soon
+
+#example queries
+
+```
+search(query: { query: "Beatles Help" }){
+  discogs {
+    releases {
+      artist
+    }
+  }
+}
+```
+
+```
+discogs(search: { query: "Beatles" }) {
+  releases {
+    artist
+  }
+}
+```
+
+```
+discogs {
+  release(id: 44444) {
+    artist
+  }
+}
+```
+
+```
+spotify {
+  album(id: 55555) {
+    artist
+    connection {
+      discogs {
+        releases {
+          artist
+        }
+      }
+    }
+  }
+}
+```
--- graphql/index.js
+++ graphql/index.js
@@ -2,23 +2,67 @@ const {  ApolloServer, gql } = require('apollo-server-koa');
 const { mergeSchemas } = require('graphql-tools');
 const { version } = require('../package');
 
-// Construct a schema, using GraphQL schema language
+
 const appTypeDefs = gql`
   type Query {
     version: String
   }
 `;
 
-// Provide resolver functions for your schema fields
 const appResolvers = {
   Query: {
     version: () => version,
   },
 };
 
+
+const searchTypeDefs = gql`
+  type DiscogsRelease {
+    artist: String
+    album: String
+  }
+
+  type DiscogsResult {
+    releases: [DiscogsRelease]
+  }
+
+
+  input SearchQuery {
+    query: String
+    artist: String
+    album: String
+  }
+
+  type SearchResult {
+    discogs: DiscogsResult
+  }
+
+  extend type Query {
+    search(query: SearchQuery): SearchResult
+    discogs: DiscogsResult
+  }
+`;
+
+const searchResolves = {
+  Query: {
+    search: (_, { query }) => {
+      return {
+        discogs: {
+          releases: []
+        }
+      }
+    },
+    discogs: () => {
+      return {
+        releases: []
+      }
+    }
+  },
+};
+
 const server = new ApolloServer({
-  typeDefs: [appTypeDefs],
-  resolvers: [appResolvers],
+  typeDefs: [appTypeDefs, searchTypeDefs],
+  resolvers: [appResolvers, searchResolves],
 });
 
 module.exports = server;
