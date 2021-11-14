adf70e7b75c284721b07bf230df3abe4c8eec016
ni-azhukov <alexander.zhukov@native-instruments.de>
2021-11-14 15:30:58 +0100

    remove book from schema

--- src/schema/root.ts
+++ src/schema/root.ts
@@ -10,37 +10,15 @@ export const typeDefs = gql`
     albums: SearchAlbums
   }
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    books: [Book]
     search: Search
   }
 `;
-
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 export const resolvers = {
   Query: {
-    books: () => books,
     search: () => ({}),
   },
   Search: {
