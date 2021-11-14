f656af3713f8972d7d001511f4328a140c8899a5
ni-azhukov <alexander.zhukov@native-instruments.de>
2021-11-14 15:29:18 +0100

    split schema

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
-enum test {
-  HELLO,
-  WORLD,
-}
+import { ApolloServer, gql } from 'apollo-server';
 
-console.log(test);
+import {
+  resolvers as rootResolvers,
+  typeDefs as rootTypeDefs,
+} from './schema/root';
+import {
+  resolvers as spotifyResolvers,
+  typeDefs as spotifyTypeDefs,
+} from './schema/spotify';
+
+const typeDefs = [rootTypeDefs, spotifyTypeDefs];
+const resolvers = [rootResolvers, spotifyResolvers];
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ src/schema/spotify.ts
@@ -0,0 +1,21 @@
+import { gql } from 'apollo-server';
+
+export const typeDefs = gql`
+  type SearchSpotifyAlbum {
+    id: ID!
+    title: String!
+  }
+
+  extend type SearchAlbums {
+    spotify: SearchSpotifyAlbum
+  }
+`;
+
+export const resolvers = {
+  SearchAlbums: {
+    spotify: () => {
+      console.log('spotify search resolver');
+      return { id: 1, title: 'test' };
+    },
+  },
+};
