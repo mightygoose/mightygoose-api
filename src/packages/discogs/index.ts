83744fb71525eab4fa5112740037784280a59cf7
ni-azhukov <alexander.zhukov@native-instruments.de>
2021-11-17 00:48:50 +0100

    update discogs types

--- src/packages/discogs/index.ts
+++ src/packages/discogs/index.ts
@@ -76,7 +76,7 @@ export const typeDefs = gql`
   }
 
   extend type Search {
-    discogs: DiscogsSearch
+    discogs: DiscogsSearch!
   }
 `;
 
--- src/packages/discogs/types.d.ts
+++ src/packages/discogs/types.d.ts
@@ -149,7 +149,7 @@ export type Relation = {
 
 export type Search = {
   __typename: 'Search';
-  discogs?: Maybe<DiscogsSearch>;
+  discogs: DiscogsSearch;
 };
 
 export type SearchDiscogsMaster = {
